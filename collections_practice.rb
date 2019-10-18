8b6f7c44124eac0a3e32ab5333bc8ccd686c1100
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-18 17:07:15 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  return array.sort 
+end 
 
+def sort_array_desc(array)
+  new_array = array.sort
+  return new_array.reverse
+end 
+
+def sort_array_char_count(array)
+  array = array.sort_by {|element| element.length}
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+
+def reverse_array(array)
+  return array.reverse
+end 
+
+def kesha_maker(array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end 
+
+def find_a(array)
+  array.select do |element|
+    element.start_with?("a")
+  end 
+end 
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n } 
+end 
+
+def add_s(array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end 
